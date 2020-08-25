d87a01eea3615aafe02bf9ecea30e20c55d15bff
Hansanghyeon <dandan9509@gmail.com>
2020-08-26 01:30:11 +0900

    feat: plugin setting page header

--- vercelHook/pages/setting.php
+++ vercelHook/pages/setting.php
@@ -2,7 +2,13 @@
 ?>
 
 <div class="wrap vercel-hook-markup">
-  <h2>Vercel Hook Settings</h2>
+  <div class="vh-header">
+    <h2>Vercel Hook Settings</h2>
+    <span class="made">
+      Made by
+    </span>
+    <a href="https://github.com/Hansanghyeon">Hansanghyeon</a>
+  </div>
   <?php settings_errors(); ?>
   <form method="POST" action="options.php">
     <?php
